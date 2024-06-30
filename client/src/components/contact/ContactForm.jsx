ed04c1ed9467e79c91f464ab952d38e1980fb9f1
hassanalee <hassanali14563@gmail.com>
2024-06-30 23:01:36 +0500

    fixed an issue

--- client/src/components/contact/ContactForm.jsx
+++ client/src/components/contact/ContactForm.jsx
@@ -3,6 +3,7 @@ import FormControl from '../form/FormControl'
 import * as Yup from "yup"
 import Grid from '../Grid'
 import { ButtonFilled } from '../Buttons'
+import Heading from '../Heading'
 const ContactForm = () => {
     const initialValues = {
         name: "",
@@ -15,20 +16,22 @@ const ContactForm = () => {
         message: Yup.string().required('Message is required')
     })
     return (
-        <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={values => console.log(values)}>
-            {
-                formik => {
-                    return <Form className="m-6">
-                        <Grid className={"justify-between"}>
-                            <FormControl control={"input"} name="name" label="Name" />
-                            <FormControl control={"input"} name="email" label="Email" type="email" />
-                        </Grid>
-                        <FormControl control="textarea" name="message" label="Message" />
-                        <ButtonFilled type="submit" text="send message" optClasses="mt-4" />
-                    </Form>
+        <>
+            <Heading text={"We would love to hear from you"} />
+            <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={values => console.log(values)}>
+                {
+                    formik => {
+                        return <Form className="m-6">
+                            <Grid className={"justify-between"}>
+                                <FormControl control={"input"} name="name" label="Name" />
+                                <FormControl control={"input"} name="email" label="Email" type="email" />
+                            </Grid>
+                            <FormControl control="textarea" name="message" label="Message" />
+                            <ButtonFilled type="submit" text="send message" optClasses="mt-4" />
+                        </Form>
+                    }
                 }
-            }
-        </Formik>
+            </Formik></>
     )
 }
 
