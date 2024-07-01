11291a594e5b2fe0c362065049607d82bea08310
hassanalee <hassanali14563@gmail.com>
2024-07-01 07:15:23 +0500

    completed contact page

--- client/src/components/contact/ContactForm.jsx
+++ client/src/components/contact/ContactForm.jsx
@@ -4,6 +4,7 @@ import * as Yup from "yup"
 import Grid from '../Grid'
 import { ButtonFilled } from '../Buttons'
 import Heading from '../Heading'
+import Text from '../Text'
 const ContactForm = () => {
     const initialValues = {
         name: "",
@@ -16,22 +17,34 @@ const ContactForm = () => {
         message: Yup.string().required('Message is required')
     })
     return (
-        <>
-            <Heading text={"We would love to hear from you"} />
-            <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={values => console.log(values)}>
-                {
-                    formik => {
-                        return <Form className="m-6">
-                            <Grid className={"justify-between"}>
-                                <FormControl control={"input"} name="name" label="Name" />
-                                <FormControl control={"input"} name="email" label="Email" type="email" />
-                            </Grid>
-                            <FormControl control="textarea" name="message" label="Message" />
-                            <ButtonFilled type="submit" text="send message" optClasses="mt-4" />
-                        </Form>
+        <Grid className={"m-6 justify-between"}>
+            <section className='w-full md:w-[70%]'>
+                <Heading text={"We would love to hear from you"} className="font-semibold text-3xl" />
+                <Text className="font-[300] text-sm my-4" text={"I you have any query or any type of suggestion you can directly contact us!"} />
+                <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={values => console.log(values)}>
+                    {
+                        formik => {
+                            return <Form>
+                                <Grid className={"justify-between"}>
+                                    <FormControl control={"input"} name="name" label="Name" />
+                                    <FormControl control={"input"} name="email" label="Email" type="email" />
+                                </Grid>
+                                <FormControl control="textarea" name="message" label="Message" />
+                                <ButtonFilled type="submit" text="send message" optClasses="mt-4" />
+                            </Form>
+                        }
                     }
-                }
-            </Formik></>
+                </Formik>
+            </section>
+            <div className='md:w-[25%] w-full'>
+                <Heading text="visit us" className="font-normal text-lg capitalize" />
+                <Text className="font-[300] text-sm" text="Islamabad, Pakistan" />
+                <Text text="Phone: +920543777" className="font-[300] text-sm" />
+                <Heading text="get in touch" className="font-normal text-lg mt-6 capitalize" />
+                <Text text="You can also contact us on this email." className="font-[300] text-sm" />
+                <Text text="Email: hassanalics72@gmail.com" className="font-[300] text-sm" />
+            </div>
+        </Grid>
     )
 }
 
