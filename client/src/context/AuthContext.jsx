6ff09bb2d0159541df515e8f2f461b6e61025172
hassanalee <hassanali14563@gmail.com>
2024-07-10 20:47:47 +0500

    added register with all steps on frontend

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -2,24 +2,31 @@ import React, { useContext, useState } from "react";
 const AuthContext = React.createContext();
 import toast from "react-hot-toast";
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 const AuthProvider = ({ children }) => {
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate()
   //   handle the signup
   const handleSignup = async (values) => {
     setLoading(true);
-    console.log(values);
-    // try {
-    //   const res = await axios.post(
-    //     "http://localhost:3000/user/register",
-    //     values
-    //   );
-    //   toast.success(res.data.message)
-    // } catch (error) {
-    //   console.log(error);
-    //   toast.error(error.message || "Something went wrong, please try again");
-    // } finally {
-    //   setLoading(false)
-    // }
+    const formData = new FormData()
+    for (let key in values) {
+      formData.append(key, values[key])
+    }
+    try {
+      const res = await axios.post(
+        "http://localhost:3000/user/register",
+        formData)
+      toast.success(res.data.message)
+      setTimeout(() => {
+        navigate('/')
+      }, 1500)
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message || "Something went wrong, please try again");
+    } finally {
+      setLoading(false)
+    }
   };
   return (
     <AuthContext.Provider value={{ loading, handleSignup }}>
